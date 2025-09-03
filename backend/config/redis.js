7445cf2da609ce2967e9444de560b6bdb24383d9
kumarharshit0413 <kumarharshit0413@gmail.com>
2025-09-03 10:55:57 +0530

    configure redis to use upstash url in production

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,11 +1,14 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
+  url: process.env.REDIS_URL 
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
-redisClient.on('connect', () => console.log('✅ Connected to Redis!'));
 
-redisClient.connect();
+(async () => {
+  await redisClient.connect();
+  console.log('✅ Connected to Redis!');
+})();
 
 module.exports = redisClient;
